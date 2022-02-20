7cd09ee0026d9015b0225f2771bb8f7778a3ceee
agencyadmin <osmprobox@gmail.com>
2022-02-20 10:38:55 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._1._5_;
+class Program
+{
+    public static void ShowAds()
+    {
+        Console.WriteLine("Поситите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+        Thread.Sleep(1000);
+        Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+        Thread.Sleep(2000);
+        Console.WriteLine("Поситите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+        Thread.Sleep(3000);
+
+    }
+
+    public static void Main(string[] args)
+    {
+        List<User> listUsr = new List<User>();
+        listUsr.Add(item: new User("papapa", "Sasha", "No"));
+        listUsr.Add(item: new User("mamama", "Ivan", "Yes"));
+        listUsr.Add(item: new User("sasasa", "Katja", "Yes"));
+        listUsr.Add(item: new User("tatata", "Nastja", "No"));
+        listUsr.Add(item: new User("jajaja", "Natasha", "Yes"));
+        listUsr.Add(item: new User("dadada", "Dasha", "No"));
+
+        foreach (User usr in listUsr)
+        {
+            Console.WriteLine("Приветствую тебя " + usr.Name + " !");
+            if (usr.IsPremium != "Yes")
+            {
+                Console.WriteLine("Напоминаем Вам, что у Вас нет до сих пор Премиум доступа!");
+                ShowAds();
+            }
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._1._5_;
+
+public class User
+{
+    public string Login { get; set; }
+    public string Name { get; set; }
+    public string IsPremium { get; set; }
+    public User(string Login, string Name, string IsPremium)
+    {
+        this.Login = Login;
+        this.Name = Name;
+        this.IsPremium = IsPremium;
+    }
+}
+
